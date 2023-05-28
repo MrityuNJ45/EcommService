13b9fc9d7575ef39727773c9ad858bc9e5b143a8
MrityuNJ45 <mrityunjaysorcar@gmail.com>
2023-05-28 21:33:00 +0530

    added client service and controller method for some services of order

--- userservice3/src/main/java/com/ecommerceapp/userservice/controllers/OrderController.java
+++ userservice3/src/main/java/com/ecommerceapp/userservice/controllers/OrderController.java
@@ -6,6 +6,8 @@ import com.ecommerceapp.userservice.service.OrderClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/order")
 public class OrderController {
@@ -18,9 +20,19 @@ public class OrderController {
         return orderClientService.createOrder(orderDTO);
     }
 
-    @GetMapping("/get/{userEmail}")
-    public Order getOrder(@PathVariable("userEmail") String email) {
-        return orderClientService.getOrder(email);
+    @GetMapping("/get/{orderId}")
+    public Order getOrder(@PathVariable("orderId") Integer orderId) {
+        return orderClientService.getOrder(orderId);
+    }
+
+    @GetMapping("/get/all/{userEmail}")
+    public List<Order> getAllOrdersOfUserHandler(@PathVariable("userEmail") String userEmail) {
+        return orderClientService.getAllOrdersOfUser(userEmail);
+    }
+
+    @GetMapping("/get/all")
+    public List<Order> getAllOrdersHandler() {
+        return orderClientService.getAllOrders();
     }
 
 }
--- userservice3/src/main/java/com/ecommerceapp/userservice/service/OrderClientService.java
+++ userservice3/src/main/java/com/ecommerceapp/userservice/service/OrderClientService.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderClientService {
@@ -21,9 +23,6 @@ public class OrderClientService {
 
     @GrpcClient("grpc-order-service")
     OrderServiceGrpc.OrderServiceBlockingStub orderServiceBlockingStub;
-
-//    @GrpcClient("grpc-product-service")
-//    ProductServiceGrpc.ProductServiceBlockingStub productServiceBlockingStub;
 
     @Autowired
     private ProductClientService productClientService;
@@ -45,14 +44,44 @@ public class OrderClientService {
         return modelResponse;
     }
 
-    public com.ecommerceapp.userservice.models.Order getOrder(String userEmail) {
-        Order.GetOrderResponse getOrderRequest = Order.GetOrderResponse.newBuilder()
-                .setUserEmail(userEmail).build();
+    public com.ecommerceapp.userservice.models.Order getOrder(Integer orderId) {
+        Order.GetOrderRequest getOrderRequest = Order.GetOrderRequest.newBuilder()
+                .setOrderId(orderId).build();
 
         Order.OrderResponse response =orderServiceBlockingStub.getOrder(getOrderRequest);
         LocalDateTime timeOfOrder = LocalDateTime.ofInstant(Instant.ofEpochSecond(response.getCreatedAt().getSeconds(), response.getCreatedAt().getNanos()), ZoneOffset.UTC);
         com.ecommerceapp.userservice.models.Order modelResponse = new com.ecommerceapp.userservice.models.Order(response.getId(), response.getProductId(), response.getUserEmail(), timeOfOrder);
         return modelResponse;
+    }
+
+    public List<com.ecommerceapp.userservice.models.Order> getAllOrdersOfUser(String email) {
+        Order.GetAllOrdersOfUserRequest getAllOrdersRequest = Order.GetAllOrdersOfUserRequest.newBuilder().setUserEmail(email).build();
+
+        List<com.ecommerceapp.userservice.models.Order> responseOrder = new ArrayList<>();
+        Order.GetAllOrdersOfUserResponse response = orderServiceBlockingStub.getAllOrdersOfUser(getAllOrdersRequest);
+        for(Order.OrderResponse order : response.getOrdersList()) {
+            LocalDateTime timeOfOrder = LocalDateTime.ofInstant(Instant.ofEpochSecond(order.getCreatedAt().getSeconds(), order.getCreatedAt().getNanos()), ZoneOffset.UTC);
+            com.ecommerceapp.userservice.models.Order modelResponse = new com.ecommerceapp.userservice.models.Order(order.getId(), order.getProductId(), order.getUserEmail(), timeOfOrder);
+            responseOrder.add(modelResponse);
+        }
+
+        return responseOrder;
+    }
+
+    public List<com.ecommerceapp.userservice.models.Order> getAllOrders() {
+
+        Order.GetAllOrdersRequest getAllOrdersRequest = Order.GetAllOrdersRequest.newBuilder().build();
+
+        List<com.ecommerceapp.userservice.models.Order> responseOrder = new ArrayList<>();
+        Order.GetAllOrdersResponse response = orderServiceBlockingStub.getAllOrders(getAllOrdersRequest);
+        for(Order.OrderResponse order : response.getOrdersList()) {
+            LocalDateTime timeOfOrder = LocalDateTime.ofInstant(Instant.ofEpochSecond(order.getCreatedAt().getSeconds(), order.getCreatedAt().getNanos()), ZoneOffset.UTC);
+            com.ecommerceapp.userservice.models.Order modelResponse = new com.ecommerceapp.userservice.models.Order(order.getId(), order.getProductId(), order.getUserEmail(), timeOfOrder);
+            responseOrder.add(modelResponse);
+        }
+
+        return responseOrder;
+
     }
 
 }
